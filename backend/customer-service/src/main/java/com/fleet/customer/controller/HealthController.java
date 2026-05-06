8172e77e515ed128d77c8ec0d70445218e887cc8
yonesyns <y.aitabid5134@uca.ac.ma>
2026-05-06 19:27:14 +0100

    api-gatway avec deux service

--- backend/customer-service/src/main/java/com/fleet/customer/controller/HealthController.java
+++ backend/customer-service/src/main/java/com/fleet/customer/controller/HealthController.java
@@ -1,12 +1,14 @@
 package com.fleet.customer.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/customers")
 public class HealthController {
 
-    @GetMapping("/customers/health")
+    @GetMapping("/health")
     public String health() {
         return "Customer Service is running";
     }
